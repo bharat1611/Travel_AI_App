f10c141ba326acd7b74f79435e145ab260eaabd6
Bharat <95923021+bharat1611@users.noreply.github.com>
2024-08-01 01:17:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwyt-6fa5zD1zZYszZyrJu4XRpYUirQsA",
-    authDomain: "rentorsell-6b825.firebaseapp.com",
-    projectId: "rentorsell-6b825",
-    storageBucket: "rentorsell-6b825.appspot.com",
-    messagingSenderId: "346106061456",
-    appId: "1:346106061456:web:1dbdb206fda2bcc876ae15",
-    measurementId: "G-2QGVSF26Y3"
+hidden
   };
 const app = initializeApp(firebaseConfig);
 
